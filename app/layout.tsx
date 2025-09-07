16e4c355cf69fb17a0132ff966d7b50dcca098ec
Oliver <oliversolomonwainaina@gmail.com>
2025-09-08 00:55:52 +0300

    Add Google Tag Manager integration to layout for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,8 +60,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}>        
-        <a href="#main" className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:m-4 focus:rounded focus:bg-foreground focus:text-background focus:px-3 focus:py-2">Skip to content</a>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZXNJWKZ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+      >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZXNJWKZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <a
+          href="#main"
+          className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:m-4 focus:rounded focus:bg-foreground focus:text-background focus:px-3 focus:py-2"
+        >
+          Skip to content
+        </a>
         <Header />
         <main id="main" className="min-h-[70vh]">
           {children}
